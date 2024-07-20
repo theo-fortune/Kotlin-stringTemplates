80d1010932bc842bd08b4e89999558994dc01466
Theo Fortune <theofortunefunction@gmail.com>
2024-07-20 21:14:02 +0100

    do while loop

--- app/src/main/java/com/typhon/expressionstest/MainActivity.kt
+++ app/src/main/java/com/typhon/expressionstest/MainActivity.kt
@@ -2,12 +2,12 @@ package com.typhon.expressionstest
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.time.LocalTime
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,40 +15,13 @@ class MainActivity : AppCompatActivity() {
 		enableEdgeToEdge()
 		setContentView(R.layout.activity_main)
 
-		val myText: TextView = findViewById(R.id.textView)
+		var myTextView: TextView = findViewById(R.id.textView)
 
-		val rating : Int = 5
+		var age = 24;
 
-		when(rating) {
-			1 -> {
-				myText.text = "Hate it! Rating = ${rating} stars"
-				myText.setTextColor((Color.parseColor("#FF0000")))
-
-			}
-			2 -> {
-				myText.text = "Not Good! Rating = ${rating} stars"
-				myText.setTextColor((Color.parseColor("#FFA500")))
-
-			}
-			3 -> {
-				myText.text = "It's Okay! Rating = ${rating} stars"
-				myText.setTextColor((Color.parseColor("#FFDA00")))
-
-			}
-			4 -> {
-				myText.text = "Good! Rating = ${rating} stars"
-				myText.setTextColor((Color.parseColor("#97FA02")))
-
-			}
-			5 -> {
-				myText.text = "Excellent! Rating = ${rating} stars"
-				myText.setTextColor((Color.parseColor("#008000")))
-
-			}
-			else -> {
-				myText.text = "Something missing"
-				myText.setTextColor((Color.parseColor("#FF0000")))
-			}
+		while (age>0) {
+			Log.i("Fortune", "$age")
+			age--
 		}
 
 		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
